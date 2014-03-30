028a75f1326e0a63af8df5532ed368b29b3e34fb
Noah D. Brenowitz <nbren12@gmail.com>
2014-03-30 17:26:28 -0400

    implemented scheme in timestep.cpp

--- hw3/main.cpp
+++ hw3/main.cpp
@@ -2,8 +2,8 @@
 
  http://www.math.nyu.edu/faculty/goodman/teaching/NumericalMethodsII2014/index.html
 
-The author gives permission for anyone to use this publically posted 
-code for any purpose.  The code was written for teaching, not research 
+The author gives permission for anyone to use this publically posted
+code for any purpose.  The code was written for teaching, not research
 or commercial use.  It has not been tested thoroughly and probably has
 serious bugs.  Results may be inaccurate, incorrect, or just wrong. */
 
@@ -24,46 +24,47 @@ using namespace std;
 #define l(j,k,nx) ((j) + ((nx)*(k)))    // Lots of harmless parens to avoid confusion
 
 int main(){
-  
+
   cout << "hello, movie world" << endl;
-  
+
 // Specified run parameters
-  
+
   const double L   = 5.;         // Length of the computational interval
   const double T   = 5.;         // Integrate up to this time
-  
+
   const int    nx  = 2000;         // number of space grid points
+  const int    neq = 2;          // Number of equations
   const double dx  = L / (nx-1); // x_0 = 0 and x_{nx-1} = L are the first and last grid points
   const double lam = .8;         // the CFL ratio
         double sMax;             // fastest wave speed in the problem, to be computed
-  
+
   const double tf  = .03;         // time interval between frames
-  
+
 //   run setup
 
   const int    nf = 1 + (int) ( T / tf ); // The number of frames, including initial condition
   const double tfa = T / nf;              // time per frame, adjusted, in case of roundoff in nf.
-  
+
   vector<double> u;          // The solution at the current time step
   vector<double> v;          // The solution at the next time step, scratch storage
   vector<double> frames;     // Each row is the u value at the corresponding frame time
-   
-  u.resize(nx);              // Allocate the memory
-  v.resize(nx);
+
+  u.resize(nx * neq);              // Allocate the memory
+  v.resize((nx+2) * neq);           // Allocate memory with ghost cells
   frames.resize(nx*(nf+1));  // With the initial data, there are nf+1 frames
-  
+
   init( u, sMax, L, .7*L, .06*L, 30., nx);           // Set initial conditions
-  
+
   double dt = lam*dx/sMax;           // time step, as determined by CFL
   int    nt = 1 + (int) ( tf / dt);  // number of time steps per frame, rounded up.
          dt = tf/nt;                 // lower dt so exactly nt time steps of size dt fit into tf
-  
+
   for ( int j = 0; j < nx; j++ ){
     frames[ l(j,0,nx)] = u[j];       // frame zero is the initial data.
    }
-  
+
   for ( int frame = 1; frame <= nf; frame++){  // to make a frame ...
-  
+
     for ( int k = 0; k < nt; k++) {                  // ... advance the solution  ...
        timeStep( u, v, dx, dt, nx);
       }
@@ -71,38 +72,38 @@ int main(){
        frames[ l( j, frame, nx)] = u[j];
       }
     }
-  
-  
+
+
 //   Write the results into a Python file
-  
-  
+
+
   ofstream runOutput;                  // Internal name of the plot file
   runOutput.open ("runOutput.py");      // Open, and give an external name
-  
+
   string pyIndent = "   ";             // One indententation in python
   runOutput <<             "import numpy as np"      << endl;
   runOutput <<             "def RunData():"          << endl;
-  
+
   runOutput << pyIndent << "L = " << L               << endl;
   runOutput << pyIndent << "data = { 'L' : L } "     << endl;    // a dictionary for all run information
-  
+
   runOutput << pyIndent << "tf = " << tf             << endl;
   runOutput << pyIndent << "data[ 'tf' ] = tf "      << endl;
-  
+
   runOutput << pyIndent << "fMin = " << -1.          << endl;
   runOutput << pyIndent << "data[ 'fMin' ] = fMin "  << endl;
-  
+
   runOutput << pyIndent << "fMax = " << 1.           << endl;
   runOutput << pyIndent << "data[ 'fMax' ] = fMax "  << endl;
-  
+
   runOutput << pyIndent << "runString = 'wave packet moves left'" << endl;
   runOutput << pyIndent << "data[ 'runString' ] = runString "  << endl;
-  
+
   pyWrite( frames, "frames", pyIndent, nf, nx, runOutput);
-  
+
   runOutput << pyIndent << "return data"             << endl;
-  
+
   runOutput.close();
-  
+
   return 0;
  }
--- hw3/timeStep.cpp
+++ hw3/timeStep.cpp
@@ -2,8 +2,8 @@
 
  http://www.math.nyu.edu/faculty/goodman/teaching/NumericalMethodsII2014/index.html
 
-The author gives permission for anyone to use this publically posted 
-code for any purpose.  The code was written for teaching, not research 
+The author gives permission for anyone to use this publically posted
+code for any purpose.  The code was written for teaching, not research
 or commercial use.  It has not been tested thoroughly and probably has
 serious bugs.  Results may be inaccurate, incorrect, or just wrong. */
 
@@ -25,23 +25,100 @@ set u[j] = u_0(x_j)
 #include <vector>         // so you can use the vector class/template system
 using namespace std;
 
+#define l(j,k,nx) ((j) + ((nx)*(k)))    // Lots of harmless parens to avoid confusion
+
+void timeStepDemo( vector<double>& u,      // the solution at the current time, replaced by the solution at the next tiome
+               vector<double>& v,      // a temp vector
+               double          dx,     // the space step
+               double          dt,     // the time step
+               int             nx) {   // number of grid points
+
+  for ( int j = 0; j < (nx-1); j++) {  // do a left shift ...
+    v[j] = u[j+1];
+   }
+  v[nx-1] = 0.;                        // ... adding a zero on the end
+
+  for ( int j = 0; j < nx; j++) {      // copy v back to u when the time step is done.
+    u[j] = v[j];
+   }
+
+  return;
+ }
+
+
+double bottom(double x) {
+    return 0.0 ;
+}
 
 void timeStep( vector<double>& u,      // the solution at the current time, replaced by the solution at the next tiome
                vector<double>& v,      // a temp vector
                double          dx,     // the space step
                double          dt,     // the time step
                int             nx) {   // number of grid points
-  
-  for ( int j = 0; j < (nx-1); j++) {  // do a left shift ...
-    v[j] = u[j+1];
-   }
-  v[nx-1] = 0.;                        // ... adding a zero on the end
-  
-  for ( int j = 0; j < nx; j++) {      // copy v back to u when the time step is done.
-    u[j] = v[j];
-   }
-  
-  return;
- }
 
-  
+
+
+    double g = 9.81;
+    double hbar = 100.0;
+    double mu  = dt/dx;
+    double up, um,u0, hp, hm,h0;
+    double bp, bm, b0, bp2, bm2;
+    int i  = 0;
+
+    // Use lax wendroff with the bottom topography
+
+    // Handle periodic BC using ghost cells
+    for (i = 1; i < nx + 1; i++)
+    {
+        v[l(i, 0, nx+2)] = u[l(i-1, 0, nx)];
+    }
+
+
+    for (i = 1; i < nx + 1; i++)
+    {
+        v[l(i, 1, nx+2)] = u[l(i-1, 1, nx)];
+    }
+
+
+    v[l(0, 0,nx+2)] = u[l(nx, 0, nx)];
+    v[l(0, 1,nx+2)] = u[l(nx, 1, nx)];
+
+    v[l(nx+1, 0,nx+2)] = u[l(0, 0, nx)];
+    v[l(nx+1, 1,nx+2)] = u[l(0, 1, nx)];
+
+
+    for (i = 1; i < nx+1; i++)
+    {
+        up = v[l(i+1,0,nx+2 )];
+        um = v[l(i-1,0,nx+2 )];
+        u0 = v[l(i,0,nx+2 )];
+
+        hp = v[l(i+1,1,nx+2 )];
+        hm = v[l(i-1,1,nx+2 )];
+        h0 = v[l(i,1,nx+2 )];
+
+        bm = bottom((i-2) * dx);
+        bp = bottom((i) * dx);
+        b0 = bottom((i-1) * dx);
+        bm2 = bottom((i-1) * dx- dx/2.0);
+        bp2 = bottom((i-1) * dx + dx/2.0);
+
+
+
+
+        // First Order terms
+        u[l(i-1, 0, nx)] -=  (mu /2.0)*( g *( hp - hm ) );
+        u[l(i-1, 1, nx)] -=  (mu /2.0)* ( (hp-bp)*up - (hm-bm)*um );
+
+
+        // Second order correction
+        u[l(i-1,0, nx)] += (mu * mu /2.0) * g * hbar *( up + um - 2.0*u0 );
+        u[l(i-1,1, nx)] += (mu * mu /2.0) * g * hbar *( hp + hm - 2.0*h0 );
+
+        u[l(i-1,0,nx)]  -= (mu * mu /2.0) * g *( bp * up + bm*um - 2.0*b0*u0 );
+        u[l(i-1,1,nx)]  -= (mu * mu/ 2.0) * g *( bp2 * (hp -h0) -bm2 * (h0-hm) );
+
+    }
+}
+
+
