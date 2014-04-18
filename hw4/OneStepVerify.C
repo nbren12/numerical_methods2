806ccb24592fac2f6c5ecb7652ad860109f5104a
Noah D. Brenowitz <nbren12@gmail.com>
2014-04-18 18:30:22 -0400

    added f and init functions, and skeleton for OneStepVerify

--- hw4/OneStepVerify.C
+++ hw4/OneStepVerify.C
@@ -2,11 +2,28 @@
 
  http://www.math.nyu.edu/faculty/goodman/teaching/NumericalMethodsII2014/index.html
 
-The author gives permission for anyone to use this publically posted 
-code for any purpose.  The code was written for teaching, not research 
+The author gives permission for anyone to use this publically posted
+code for any purpose.  The code was written for teaching, not research
 or commercial use.  It has not been tested thoroughly and probably has
 serious bugs.  Results may be inaccurate, incorrect, or just wrong. */
 
 
 // Source file OneStepVerify.C
 
+#include <math.h>
+#include <iostream>
+using namespace std;
+
+#include "header.h"
+
+int main(int argc, char** argv){
+
+    double *x;
+
+    x = new double(2);
+
+    init(x);
+    cout << x[0] << " " << x[1] << endl;
+
+    return 0;
+}
--- hw4/f2D.C
+++ hw4/f2D.C
@@ -2,14 +2,48 @@
 
  http://www.math.nyu.edu/faculty/goodman/teaching/NumericalMethodsII2014/index.html
 
-The author gives permission for anyone to use this publically posted 
-code for any purpose.  The code was written for teaching, not research 
+The author gives permission for anyone to use this publically posted
+code for any purpose.  The code was written for teaching, not research
 or commercial use.  It has not been tested thoroughly and probably has
 serious bugs.  Results may be inaccurate, incorrect, or just wrong. */
 
 
 // Source file f2D.C
 
+/*  Problem specific code for an n-body gravitational interaction simulation
+
+There are two procedures,
+
+init: initialized variables and sets initial configuration
+
+f:   computes the time derivatives of the positions and velocities
+
+*/
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+void init(double x0[]) {
+    x0[0] = 0;
+    x0[1] = 1;
+}
+
+void f( double f[], double x[]){
+
+    double xsq;
+
+    xsq = x[0]*x[0] + x[1]*x[1];
+
+    f[0] = ( 1 + xsq ) * x[1];
+    f[1] = - ( 1 + xsq ) * x[0];
+
+  }
+
+void exact_soln (double x[], double t){
+    x[0] = sin( 2.0 * t );
+    x[1] = cos( 2.0 * t );
+}
 
 
 
@@ -20,6 +54,18 @@ serious bugs.  Results may be inaccurate, incorrect, or just wrong. */
 
 
 
-  
 
-  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
