a2a87f254d18d85f3d08754646253fbc9a7a9cf1
Noah D. Brenowitz <nbren12@gmail.com>
2014-04-22 16:28:28 -0400

    FixedStepVerify is complete

--- hw4/FixedStepVerify.C
+++ hw4/FixedStepVerify.C
@@ -23,12 +23,14 @@ using namespace std;
 int main(){
 
     cout << "Fixed Step Verify:" << endl;
+    cout << "For order 3 RK method, we expect the ratio to be 2^3=8" << endl << endl;
 
     int n = 2;
+    int i,j,k;
 
     double T  = 4.0 * PI;
     double dt = T / 1000;
-    double nIter = 2;
+    const double nIter = 15;
 
     double *x, *dx, *x_exact;
     double *v1, *v2, *v3;
@@ -42,23 +44,40 @@ int main(){
 
     int nt = 10;
     int ntTmp = 10;
+    double err1, err0;
 
-    for (int i = 0; i < nIter; i++)
+    for (k = 0; k < nIter; k++)
     {
-        ntTmp = nt * 2;
+        ntTmp = nt * 2;  // double the number of time steps
         dt    = T / ntTmp;
 
         init(x);                     //Initialize the routine
 
-        for ( int i = 0; i < nt; i++){
+        // Time step up until T
+        for (i = 0; i < nt; i++){
             RK3( dx, x, dt, n, v1, v2, v3);
-            for ( int j = 0; j < n; j++){
+            for (j = 0; j < n; j++){
                 x[j] += dx[j];
             }
         }
 
-        // TODO : Calculate error and ratio
+        exact_soln(x_exact, T);  // Load exact solution
 
+        // Calculate L2 norm error compared to exact soln
+        err1 = 0.0;
+        for (j = 0; j < n; j++)
+            err1 += pow(x[j] - x_exact[j], 2);
+
+        err1 = sqrt(err1);
+
+
+        // print convergence ratios
+        if (k  > 0 )
+            cout << "Step Size is "<<  dt << " Error is " << err1 << " Ratio is " << err0 / err1 << endl;
+
+
+        err0 = err1;
+        nt   = ntTmp;
 
     }
   return 0;
